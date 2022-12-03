3e8b04d4628b792d44a479042f272ce002ad9718
h2lo <olusha.98@mail.ru>
2022-12-04 00:49:09 +0300

    add hw8 task solution

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -57,6 +57,7 @@ const HW8 = () => {
         <div id={'hw3'}>
             <div className={s2.hwTitle}>Homework #8</div>
             <div className={s2.hw}>
+                <hr/>
                 <div className={s.container}>
                     <div className={s.buttonsContainer}>
                         <SuperButton
@@ -84,7 +85,7 @@ const HW8 = () => {
 
                     <table id={'hw8-users'} className={s.users}>
                         <thead className={s.thead}>
-                        <tr>
+                        <tr className = {s.trow}>
                             <td className={s.nameCol}>Name</td>
                             <td className={s.ageCol}>Age</td>
                         </tr>
