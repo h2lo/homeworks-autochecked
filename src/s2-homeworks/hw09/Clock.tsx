52cdd0eac064a18a59e311d4538e2d7765168893
h2lo <olusha.98@mail.ru>
2023-01-31 15:14:32 +0300

    add new way of day and month generation

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -12,11 +12,16 @@ function Clock() {
     const start = () => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-        stop()
+
+        stop() // останавливаем предыдущий таймер
+
+        // запускаем новый интервал, который примает какую-то f, выполняемую раз в какой-то промежуток времени
+        // и возвращает - «идентификатор таймера» id, который можно использовать для отмены дальнейшего выполнения
         const id: number = +setInterval(() => {
             setDate(new Date())
         }, 1000)
-        setTimerId(id)
+
+        setTimerId(id) // сохраним идентификатор запущенного таймера, чтобы потом отсановать его же в clearInterval()
     }
 
     const stop = () => {
@@ -32,19 +37,24 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = date?.toLocaleTimeString('ru-Ru')|| <br/>
-    // const stringTime = date.toTimeString().slice(0, 8)
-    // const stringTime = date?.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime = date?.toLocaleTimeString('ru-Ru') || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = date?.toLocaleDateString('ru-Ru') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    // const stringDay = 'date->day' || <br/> // пишут студенты
-    // const stringMonth = 'date->month' || <br/> // пишут студенты
 
-    const stringDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] || <br/>
-    const stringMonth = ['January', 'February', 'March', 'April', ' May', 'June', 'July', 'August', 'September', 'October', 'November', ' December'] ||
-        <br/>
+    // 1 способ
+    let formatterDay = new Intl.DateTimeFormat('en', {weekday: 'long'})
+    const stringDay = formatterDay.format(date) || <br/>
 
+    // 2 способ
+    // const stringDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] || <br/>
+    // <span id={'hw9-day'}>{stringDay[date.getDay()]}</span>,{' '}
+
+    let formatterMonth = new Intl.DateTimeFormat('en', {month: 'long'})
+    const stringMonth = formatterMonth.format(date) || <br/>
+
+    // const stringMonth = ['January', 'February', 'March', 'April', ' May', 'June', 'July', 'August', 'September', 'October', 'November', ' December'] || <br/>
+    // <span id={'hw9-month'}>{stringMonth[date.getMonth()]}</span>,{' '}
 
     return (
         <div className={s.clock}>
@@ -54,7 +64,7 @@ function Clock() {
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                <span id={'hw9-day'}>{stringDay[date.getDay()]}</span>,{' '}
+                <span id={'hw9-day'}>{stringDay}</span>,{' '}
                 <span id={'hw9-time'}>
                     <strong>{stringTime}</strong>
                 </span>
@@ -64,7 +74,7 @@ function Clock() {
                 <div className={s.more}>
                     {show ? (
                         <>
-                            <span id={'hw9-month'}>{stringMonth[date.getMonth()]}</span>,{' '}
+                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
                             <span id={'hw9-date'}>{stringDate}</span>
                         </>
                     ) : (
